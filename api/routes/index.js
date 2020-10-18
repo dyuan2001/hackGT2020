6a9bf3f759468406fcfe8e40a6f874598909ad09
Angela Dai <zhjh.angela@gmail.com>
2020-10-18 06:15:58 -0400

    minor bug fixes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const {ensureAuthenticated} = require("../config/auth.js")
-router.get('/', function(req,res)=>{
+router.get('/', (req,res)=>{
     res.render('welcome');
 })
 //register page
